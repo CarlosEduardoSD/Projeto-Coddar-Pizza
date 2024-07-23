6ad29f3ec74d86db353c199ab6d33232514e2e29
Carlos Eduardo <carloseduardosilva5858@gmail.com>
2024-07-23 20:35:24 -0300

    feat: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/js/bootstrap.min.js';
+import {RouterProvider} from 'react-router-dom'
+import { router } from './routes.jsx'
 
 function App() {
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+  {
+    
+  },
+  {
+   
+  },
+]);
