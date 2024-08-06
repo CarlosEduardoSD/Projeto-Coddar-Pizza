006e78a284cdcc4d834584ba408edc0db169c9ce
Raphael Maia <raphamaia9@gmail.com>
2024-08-06 19:25:34 -0300

    feat: create banner

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,12 @@
 import Header from "../../components/Header/header";
+import Banner from "../../components/Banner/banner";
 
 export default function Home() {
   return (
+    <>
     <Header />
+    <Banner />
+    </>
   )
 }
 
