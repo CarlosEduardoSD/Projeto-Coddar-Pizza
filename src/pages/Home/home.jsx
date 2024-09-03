30156b1946861d5e2651a8fd54c03b0dd26bd572
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-09-03 19:22:31 -0300

    feat: responsivo bannerPizzaria

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,17 +1,17 @@
 import Header from "../../components/Header/header";
 import OurMenu from "../../components/Menu/OurMenu";
 import Banner from "../../components/Banner/banner";
-import BannerPizzaria from "../../components/BannerPizzaria/BannerPizzaria";
+// import BannerPizzaria from "../../components/BannerPizzaria/BannerPizzaria";
 
 export default function Home() {
   return (
     <>
 
 
-    {/* <Header /> */}
-    {/* <Banner /> */}
-    {/* <OurMenu /> */}
-    <BannerPizzaria />
+    <Header />
+    <Banner />
+    <OurMenu />
+    {/* <BannerPizzaria /> */}
 
     </>
   );
