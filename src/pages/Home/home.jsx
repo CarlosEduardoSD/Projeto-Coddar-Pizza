beab08628c48ffc7878f6fa5281e581053c97ed8
Raphael Maia <raphamaia9@gmail.com>
2024-09-06 16:40:12 -0300

    feat: responsivo do componete pizza

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,15 +1,17 @@
 import Header from "../../components/Header/header";
-import OurMenu from "../../components/Menu/OurMenu";
-import Banner from "../../components/Banner/banner";
-import BannerPizzaria from "../../components/BannerPizzaria/BannerPizzaria";
+// import OurMenu from "../../components/Menu/OurMenu";
+// import Banner from "../../components/Banner/banner";
+// import BannerPizzaria from "../../components/BannerPizzaria/BannerPizzaria";
+import Pizza from "../../components/pizzas/pizza";
 
 export default function Home() {
   return (
     <>
-    <Header />
-    <Banner />
-    <OurMenu />
-    <BannerPizzaria />
+      <Header />
+      {/* <Banner /> */}
+      {/* <OurMenu /> */}
+      {/* <BannerPizzaria /> */}
+      <Pizza />
     </>
   );
 }
