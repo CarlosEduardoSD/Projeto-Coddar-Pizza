0c3d969b3741760e147982e77a9b1cb7446f41ba
Carlos Eduardo <carloseduardosilva5858@gmail.com>
2024-09-20 19:03:06 -0300

    Att

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -5,13 +5,30 @@ import BannerPizzaria from "../../components/BannerPizzaria/BannerPizzaria";
 import Pizza from "../../components/pizzas/pizza";
 import Lojas from "../../components/Lojas/lojas"
 
+import { useState } from "react";
+
 export default function Home() {
+  const [lojaSelecionada, setLojaSelecionada] = useState(null);
+  
+  const handleClick = (loja) => {
+    setLojaSelecionada(loja);
+  };
+
   return (
     <>
-    <Header />
-    <Banner />
-    <OurMenu />
-    <Lojas />
+      <Header />
+      {lojaSelecionada ? (
+        <>
+          <BannerPizzaria loja={lojaSelecionada}/>
+          <Pizza loja={lojaSelecionada}/>
+        </>
+      ) : (
+        <>
+          <Banner />
+          <OurMenu />
+          <Lojas onLojaClick={handleClick} />
+        </>
+      )}
     </>
   );
 }
