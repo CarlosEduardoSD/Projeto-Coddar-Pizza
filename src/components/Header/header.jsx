41beaeb90a5bae30474252c566be5fbfc584f6f9
Carlos Eduardo <carloseduardosilva5858@gmail.com>
2024-10-15 12:20:50 -0300

    feat: logout/login functions

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -7,12 +7,19 @@ import Perfil from "../../assets/img/Perfil.png";
 
 import { FaCartArrowDown } from "react-icons/fa6";
 
+import Cookies from 'js-cookie';
+
 export default function Header() {
   return (
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <h1 className={styles.header_title} href="/" style={{cursor: "pointer"}} onClick={() => window.location.reload()}>
+          <h1
+            className={styles.header_title}
+            href="/"
+            style={{ cursor: "pointer" }}
+            onClick={() => window.location.reload()}
+          >
             Codar Pizzaria
           </h1>
           <button
@@ -37,7 +44,11 @@ export default function Header() {
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className={`${styles.menu_link} ${styles.cart}`} aria-current="page" href="#">
+                <a
+                  className={`${styles.menu_link} ${styles.cart}`}
+                  aria-current="page"
+                  href="#"
+                >
                   Perfil
                   <img
                     className={styles.sidebar_img}
@@ -77,15 +88,37 @@ export default function Header() {
                 </a>
               </li>
               <li className="nav-item">
-                <a  className={`${styles.menu_link} ${styles.cart}`} aria-current="page" href="#">
+                <a
+                  className={`${styles.menu_link} ${styles.cart}`}
+                  aria-current="page"
+                  href="#"
+                >
                   Carrinho
                 </a>
               </li>
             </ul>
             <div className={styles.button_area} id="#navbarNav">
-              <div className={styles.home_img}>
-                <img className="img-fluid" src={Perfil} alt="Foto Perfil" />
+              <div className="dropdown">
+                <button
+                  className="btn"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ padding: "0px", border: "none" }}
+                >
+                  <div className={styles.home_img}>
+                    <img className="img-fluid" src={Perfil} alt="Foto Perfil" />
+                  </div>
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" onClick={() => {Cookies.remove('token'), window.location.reload()}} style={{ cursor: "pointer" }}>
+                      Logout
+                    </a>
+                  </li>
+                </ul>
               </div>
+
               <div
                 className={styles.divider_vertical}
                 style={{
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,80 +1,128 @@
-import React from 'react';
-import styles from './Login.module.css';
-import appleicone from '../../assets/icons/AppleIcon.svg';
-import googleicone from '../../assets/icons/PlayStoreIcon.svg';
+import React from "react";
+import styles from "./Login.module.css";
+import appleicone from "../../assets/icons/AppleIcon.svg";
+import googleicone from "../../assets/icons/PlayStoreIcon.svg";
+
+import Cookies from "js-cookie";
+
+import { useRef } from "react";
+
+import { api } from "../../lib/api";
 
 const Login = () => {
-    return (
-        <div className={styles.bodyLogin}>
-            <div className={styles.pageLogin}>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-6'>
-                            <div className={styles.loginTitulo}>
-                                <div className={styles.loginTituloTopo}>
-                                    <h1>Codar Pizzaria</h1>
-                                    <h2>Pegue uma fatia</h2>
-                                </div>
-                                
-                                    <div className={styles.loginIcones}>
-                                        <button className={styles.loginDownloadApple}>
-                                            <img className={styles.iconeApple} src={appleicone} alt="" />
-                                            <div className={styles.textosApple}> 
-                                                <p className={styles.textoApple1}>Download on the</p>
-                                                <p className={styles.textoApple2}>App Store</p> 
-                                            </div>  
-                                        </button>                                       
+  const email = useRef();
+  const password = useRef();
 
-                                        <button className={styles.loginDownloadGoogle}>
-                                        <img className={styles.iconeGoogle} src={googleicone} alt="" />
-                                            <div className={styles.textosGoogle}> 
-                                                <p className={styles.textoGoogle1}>Get it on</p>
-                                                <p className={styles.textoGoogle2}>Google Play</p> 
-                                            </div>
-                                        </button>
-                                                                          
+  const handleChange = async (e) => {
+    e.preventDefault();
 
-                                    </div>
-                                
-                            </div>
-                        </div>
-
-                        <div className='col-md-6'>
-                            <div className={styles.loginParte2}>
-                                <h2>Efetuar Login</h2>
-                                <hr />
-                                <span className={styles.loginLinha}></span>
-
-                                <form className={styles.formularioBox} action="">
-                                    <div className={styles.inputBoxPrincipal}>
-                                        <div className={styles.loginInputBox}>
-                                            <input type="email" name="email" id="email" placeholder='E-mail'  required />
-                                        </div>
-                                        <div className={styles.loginInputBox}>
-                                            <input type="password" name="password" id="password" placeholder='Senha' required />
-                                        </div>
-                                    </div>
-                                    <div className={styles.senha}>
-                                        <a href="#">Esqueceu sua senha?</a><br />
-                                    </div>
-                                    <button className={styles.botaoBox}>Acessar</button><br />
-                                    
-                                    <div className={styles.conta}>
-                                        <a href="/register">Criar uma conta</a>
-                                    </div>
-                                </form>
-                                <div className={styles.loginFooter}>
-                                    <p>Codar Pizzaria 2020</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+    const userData = {
+      email: email.current.value,
+      senha: password.current.value,
+    };
+    const response = await api.post("/login-user", userData);
+    console.log(response);
+    if (response.status === 200) {
+      Cookies.set("token", response.data.id, {
+        expires: 1, // 1 days
+        path: "/",
+      });
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 2000 /*1 second*/);
+    }
+  };
+  return (
+    <div className={styles.bodyLogin}>
+      <div className={styles.pageLogin}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className={styles.loginTitulo}>
+                <div className={styles.loginTituloTopo}>
+                  <h1>Codar Pizzaria</h1>
+                  <h2>Pegue uma fatia</h2>
                 </div>
+
+                <div className={styles.loginIcones}>
+                  <button className={styles.loginDownloadApple}>
+                    <img
+                      className={styles.iconeApple}
+                      src={appleicone}
+                      alt=""
+                    />
+                    <div className={styles.textosApple}>
+                      <p className={styles.textoApple1}>Download on the</p>
+                      <p className={styles.textoApple2}>App Store</p>
+                    </div>
+                  </button>
+
+                  <button className={styles.loginDownloadGoogle}>
+                    <img
+                      className={styles.iconeGoogle}
+                      src={googleicone}
+                      alt=""
+                    />
+                    <div className={styles.textosGoogle}>
+                      <p className={styles.textoGoogle1}>Get it on</p>
+                      <p className={styles.textoGoogle2}>Google Play</p>
+                    </div>
+                  </button>
+                </div>
+              </div>
             </div>
 
-        </div>
-    )
-  };
+            <div className="col-md-6">
+              <div className={styles.loginParte2}>
+                <h2>Efetuar Login</h2>
+                <hr />
+                <span className={styles.loginLinha}></span>
 
-export default Login
+                <form className={styles.formularioBox} onSubmit={handleChange}>
+                  <div className={styles.inputBoxPrincipal}>
+                    <div className={styles.loginInputBox}>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="E-mail"
+                        required
+                        ref={email}
+                      />
+                    </div>
+                    <div className={styles.loginInputBox}>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Senha"
+                        required
+                        ref={password}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.senha}>
+                    <a href="#">Esqueceu sua senha?</a>
+                    <br />
+                  </div>
+                  <button className={styles.botaoBox} type="submit">
+                    Acessar
+                  </button>
+                  <br />
+                </form>
+                <div className={styles.conta}>
+                  <a href="/register">Criar uma conta</a>
+                </div>
+                <div className={styles.loginFooter}>
+                  <p>Codar Pizzaria 2020</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
